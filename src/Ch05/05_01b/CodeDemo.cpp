a99bd2d0abefb2ee817812b2bfcb3398159376d8
PatrikP24 <patrikpaa03@gmail.com>
2026-02-05 17:47:15 +0000

    Osio 5. Tehty

--- src/Ch05/05_01b/CodeDemo.cpp
+++ src/Ch05/05_01b/CodeDemo.cpp
@@ -5,9 +5,33 @@
 #include <iostream>
 
 int main(){
-    int score = 1023;
+    int score = 926;
     bool gamePaused = false;
-    char action = 'x';
+    char action = 'A';
+    
+    if (score > 1000)
+        std::cout << "Your score is over 1000." << std::endl;
+    else
+        std::cout << "Your score is under 1000." << std::endl;
+    if (score % 2)
+        std::cout << "Score is odd" << std::endl;
+    
+    else
+        std::cout << "Score is even" << std::endl;
+
+    std::cout << "The action " << action << ", is ";
+
+    if(action != 'a' && action != 'w' && action != 'd' && action != 's' &&
+       action != 'A' && action != 'W' && action != 'D' && action != 'S')
+    {
+        std::cout << "not ";
+    }
+    std::cout << "a movement key" << std::endl;
+    
+    if (gamePaused)
+        std::cout << "Game is paused" << std::endl;
+    else
+        std::cout << "Game is not paused" << std::endl;
     
     std::cout << std::endl << std::endl;
     return 0;
--- src/Ch05/05_09/CodeDemo.cpp
+++ src/Ch05/05_09/CodeDemo.cpp
@@ -14,11 +14,49 @@ struct Resource{
 
 double CalculateTotalCost(std::vector<Resource> resources){
     double result = 0.0;
-    
+    float tax;
     // Write your code here
+    for(int i = 0; i < resources.size(); i++){
+        
+        switch(resources[i].type){
+            case 'B':
+                tax = 1.05f;
+                break;
+            case 'L':
+                tax = 1.15f;
+                break;
+            case 'E':
+                tax = 1.0f;
+                break;
+            default:
+                tax = 1.0f;
+                break;
+        }
+
+        result += resources[i].baseCost * tax;
+
+    }
     
     return result;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main(){
     
